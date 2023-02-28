789a1bd9b81c6f397beff22d6ca108832415d7f8
Ankur-Vekariya <ankurvekariya2001@gmail.com>
2023-02-28 23:06:44 +0530

    Home screen

--- App.js
+++ App.js
@@ -7,22 +7,64 @@ import {
   Text,
   View,
 } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import Colors from "./Colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ScrollView style={{ width: "100%" }}>
-        <View style={{ height: 2000 }}>
-          <Image
-            style={{ width: "100%", height: 500 }}
-            source={require("./assets/ankur.jpg")}
-          />
-
-          <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ height: "50%" }}>
+        <View
+          style={{
+            position: "absolute",
+            top: "10%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            // backgroundColor: "red",
+            width: "100%",
+            paddingHorizontal: 20,
+          }}
+        >
+          <Text
+            style={{
+              alignItems: "center",
+              verticalAlign: "middle",
+              fontSize: 20,
+              color: Colors.accent,
+            }}
+          >
+            Ankur Vekariya
+          </Text>
+          <Entypo name="menu" size={30} color={Colors.accent} />
         </View>
-      </ScrollView>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      {/* <StatusBar style="auto" /> */}
+        <Image
+          style={{ width: "100%", height: 500, opacity: 0.3 }}
+          source={require("./assets/ankur.jpg")}
+        />
+      </View>
+      <View
+        style={{
+          borderRadius: 40,
+          backgroundColor: Colors.accent,
+          width: "100%",
+          height: "100%",
+          paddingHorizontal: 50,
+          paddingVertical: 50,
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 10,
+            backgroundColor: Colors.main,
+            width: "100%",
+            height: 100,
+          }}
+        >
+          <Text>Ankur vekariya</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -30,10 +72,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    // borderWidth: 2,
-    // borderColor: "red",
+    backgroundColor: Colors.main,
   },
 });
